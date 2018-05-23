37d35967468e9692407aae18f4350f7df83a5518
khaledm356 <khaledm356@gmail.com>
2018-05-23 15:19:14 +0200

    Controlling state

--- src/Note.js
+++ src/Note.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react'
 import FaPencil from 'react-icons/lib/fa/pencil'
 import FaTrash from 'react-icons/lib/fa/trash'
+import FaFloppyO from 'react-icons/lib/fa/floppy-o'
 
 class Note extends Component {
   constructor(props){
     super(props)
+    this.state = {
+      editing : false
+    }
     this.edit = this.edit.bind(this)
     this.remove = this.remove.bind(this)
+    this.renderForm = this.renderForm.bind(this)
+    this.renderDisplay = this.renderDisplay.bind(this)
+    this.save = this.save.bind(this)
   }
 
   edit(){
-    alert("Editing Note")
+    this.setState({
+      editing : true
+    })
   }
+
   remove(){
     alert("Removing Note")
   }
-  render() {
+
+  save(){
+    alert("Saved!")
+  }
+  renderForm(){
+    return (
+      <div className="note">
+        <p>Learn React</p>
+          <form>
+            <textarea />
+            <button onClick={this.save}><FaFloppyO /></button>
+          </form>
+      </div>
+    )
+  }
+  renderDisplay() {
       return (
         <div className="note">
           <p>Learn React</p>
@@ -25,6 +50,9 @@ class Note extends Component {
             </span>
         </div>
       )
+  }
+  render(){
+    return this.state.editing ? this.renderForm() : this.renderDisplay()
   }
 }
 
